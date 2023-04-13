d53ea5a233167bebd6adca7a55f90d014a937a79
tuananhwhlh <anhmhwhlh@gmail.com>
2023-04-13 12:36:52 +0700

    MYD-2: Apply devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_current_user
+  skip_before_action :verify_authenticity_token
+
+  protected
+
+  def initialize_current_user
+    return unless user_signed_in?
+
+    Current.current_user = current_user
+  end
 end
