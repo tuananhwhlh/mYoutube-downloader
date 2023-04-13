e3249fe1575805088f8b17ae365c0c9f18bff235
Anh Do <anh.do@readytech.io>
2023-04-13 11:17:23 +0700

    MYD-1: Apply view_component gem (#1)

---
+++ test/components/home/index_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Home::IndexComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Home::IndexComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
